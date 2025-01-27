e178b707a382c032d453e8b0e21b81d6b6b65818
Athisruth <athisruthmsatheesh98@gmail.com>
2025-01-27 13:07:34 +0530

    "modules created for javascript"

--- js_pro/math.js
+++ js_pro/math.js
@@ -1,7 +1,4 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
+import { add, pi } from './main.js';
 
-const pi = 3.14159;
-
-export default { add, pi };
+console.log(add(2, 3));
+console.log(pi);        
